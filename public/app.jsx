372f961f9fafde22bd5cf8a03599a82b20245bc5
Dixit Patel <talkwithdixit@gmail.com>
2016-08-29 22:31:53 -0400

    added props and default props to the React Component

--- public/app.jsx
+++ public/app.jsx
@@ -1,20 +1,30 @@
 var Greeter = React.createClass({
+	// added defaultProps is we don't pass any props to the Greeter Component
+	getDefaultProps : function() {
+		return {
+			name : 'WebPack',
+			message: 'This is From Greeter Component'
+		}
+	},
   render: function () {
   	console.log(this); // very very important
-  	var name = this.props.name;
+  	var name = this.props.name,
+  	 	message = this.props.message;
 
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>This is form a component!</p>
+        <p>{message + ' and Use it with the String Concatenation!'}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Dixit Patel';
+
 // render the Component called Greeter , name is the prop is the HTML attribute
 ReactDOM.render(
-  <Greeter name={'ProTek'} />,
+  <Greeter name={firstName} message={'Message from Props'} />,
   document.getElementById('app')
 );
 
