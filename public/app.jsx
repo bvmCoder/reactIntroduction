cc8455f19133ceabc06e8a61a985347a72d8630e
Dixit Patel <talkwithdixit@gmail.com>
2016-08-29 22:21:00 -0400

    Added props to the react component

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,21 @@
 var Greeter = React.createClass({
   render: function () {
+  	console.log(this); // very very important
+  	var name = this.props.name;
+
     return (
       <div>
-        <h1>Hello WebPack Wolf</h1>
+        <h1>Hello {name}!</h1>
         <p>This is form a component!</p>
       </div>
     );
   }
 });
 
+// render the Component called Greeter , name is the prop is the HTML attribute
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={'ProTek'} />,
   document.getElementById('app')
 );
+
+// props is short for properties, is the way to pass the data into your component
